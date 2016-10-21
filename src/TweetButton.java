9e24aaa1c8a26cfa3f98f3ae516c3a3419af08f5
Mohamad Kalache <mohamad.kalache@student.uts.edu.au>
2016-10-21 15:28:53 +1100

    more twitter WIP

--- src/TweetButton.java
+++ src/TweetButton.java
@@ -4,6 +4,7 @@ public class TweetButton extends Button{
     
     public TweetButton(float x, float y, float w, float h, int id, int candidate) {
         super(x, y, w, h, id);
+        textSize = 10;
         tweet = BirdBrains.TWITS.getTweet(candidate);
         text = tweet.getMessage();
     }
