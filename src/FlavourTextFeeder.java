73785bf83fb28be3ff368703fcca2422c5a285c9
J-Mo <jdmoallem@gmail.com>
2016-10-26 16:25:15 +1100

    bunch of stuff

--- src/FlavourTextFeeder.java
+++ src/FlavourTextFeeder.java
@@ -1,7 +1,37 @@
-package PACKAGE_NAME;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Random;
 
-/**
- * Created by J-Mo on 26/10/2016.
- */
 public class FlavourTextFeeder {
+    Random random = new Random();
+    private ArrayList<String> trumpList = new ArrayList<>();
+    private ArrayList<String> hillaryList = new ArrayList<>();
+
+    public FlavourTextFeeder() throws Exception{
+        loadNames("src/data/TrumpQuotes.txt", trumpList);
+        loadNames("src/data/HillaryQuotes.txt", trumpList);
+    }
+
+    private void loadNames(String fileName, ArrayList<String> arrayList) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName));
+        try {
+            String line = bufferedReader.readLine();
+            while (line != null) {
+                arrayList.add(line);
+                line = bufferedReader.readLine();
+            }
+        }
+        finally {
+            bufferedReader.close();
+        }
+    }
+
+    public String getQuote(int candidate) {
+        if (candidate == 0) {
+            return hillaryList.get(random.nextInt(hillaryList.size()));
+        }
+        return trumpList.get(random.nextInt(trumpList.size()));
+    }
 }
